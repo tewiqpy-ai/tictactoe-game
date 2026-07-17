cbca99bfd704882e50d117060544163c2c0ffe78
tewiqpy-ai <tewiqpy-ai@users.noreply.github.com>
2026-07-17 21:26:58 +0700

    fix: add missing exports to firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,3 +1,7 @@
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js';
+import { getAuth, GoogleAuthProvider } from 'https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js';
+import { getFirestore } from 'https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js';
+
 const firebaseConfig = {
   apiKey: "AIzaSyDylaym85cTkafDl5OitpEZKrUuwwJHn_w",
   authDomain: "tictactoe-5ba18.firebaseapp.com",
@@ -7,12 +11,10 @@ const firebaseConfig = {
   appId: "1:23885470883:web:3289b4378c236767a47316",
   measurementId: "G-Y87Y2WPMEW"
 };
-// Firebase SDK v10 (modular, CDN)
-import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js';
-import { getAuth, GoogleAuthProvider } from 'https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js';
-import { getFirestore } from 'https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js';
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
 const googleProvider = new GoogleAuthProvider();
+
+export { auth, db, googleProvider };
